68b78dc1098501e0242d4a57afe9a0497bc002e7
Martin Caetano <pelcasandra@me.com>
2017-05-17 20:56:05 +0700

    Whitelist :merge value.

--- lib/sass/script/functions.rb
+++ lib/sass/script/functions.rb
@@ -1914,7 +1914,7 @@ MESSAGE
     # @return [Sass::Script::Value::List]
     def join(list1, list2, separator = identifier("auto"))
       assert_type separator, :String, :separator
-      unless %w(auto space comma).include?(separator.value)
+      unless %w(auto space comma merge).include?(separator.value)
         raise ArgumentError.new("Separator name must be space, comma, or auto")
       end
       sep = if separator.value == 'auto'
@@ -1950,7 +1950,7 @@ MESSAGE
     # @return [Sass::Script::Value::List]
     def append(list, val, separator = identifier("auto"))
       assert_type separator, :String, :separator
-      unless %w(auto space comma).include?(separator.value)
+      unless %w(auto space comma merge).include?(separator.value)
         raise ArgumentError.new("Separator name must be space, comma, or auto")
       end
       sep = if separator.value == 'auto'
